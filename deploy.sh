f5315ce099744875084e9ef661fbfe08158c4f76
BGalymbek <200103023@stu.sdu.edu.kz>
2024-06-13 07:10:22 +0500

    deploy.sh added

--- deploy.sh
+++ deploy.sh
@@ -3,78 +3,23 @@
 set -e
 
 # Переменные
-REPO_URL="https://github.com/BGalymbek/FrontAmazon.git"
-APP_DIR="/var/www/html/myapp"
-NGINX_CONFIG="/etc/nginx/sites-available/default"
+IMAGE_NAME="***/frontamazon:latest"
+CONTAINER_NAME="myapp"
 
-echo "Deleting old app..."
-sudo rm -rf /var/www/html/*
+echo "Pulling the latest image..."
+docker pull $IMAGE_NAME
 
-echo "Creating app folder..."
-sudo mkdir -p $APP_DIR
-sudo chown -R $EC2_USERNAME:$EC2_USERNAME $APP_DIR
+echo "Stopping the existing container..."
+docker stop $CONTAINER_NAME || true
 
-echo "Cloning the latest version of the code..."
-sudo git clone $REPO_URL $APP_DIR
+echo "Removing the existing container..."
+docker rm $CONTAINER_NAME || true
 
-cd $APP_DIR
-
-echo "Installing Node.js and npm..."
-if ! command -v node > /dev/null; then
-    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-fi
-
-echo "Installing project dependencies..."
-sudo rm -rf node_modules
-sudo rm -f package-lock.json
-sudo npm install
-sudo npm install pm2@latest -g
-sudo npm install react-scripts@latest
-
-echo "Building the project..."
-sudo npm run build
-
-echo "Serving the build with PM2..."
-sudo pm2 serve build/ 80 --spa
-
-echo "Moving build to root directory..."
-sudo mv build/* /var/www/html/
-
-echo "Configuring Nginx..."
-if ! command -v nginx > /dev/null; then
-    echo "Installing Nginx..."
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
-
-echo "Removing old Nginx configuration..."
-sudo rm -f $NGINX_CONFIG
-
-echo "Creating new Nginx configuration..."
-sudo bash -c "cat > $NGINX_CONFIG <<EOF
-server {
-    listen 80;
-    server_name _;
-
-    location / {
-        root /var/www/html;
-        index index.html index.htm;
-        try_files \$uri \$uri/ /index.html;
-    }
-}
-EOF"
-
-echo "Restarting Nginx..."
-sudo systemctl restart nginx
-
-# Очистка Docker, чтобы освободить место
+# Очистка Docker для освобождения места
 echo "Pruning Docker system to free up space..."
-sudo docker system prune -a -f
+docker system prune -a -f
 
-echo "Saving PM2 process list and enabling startup..."
-cd $APP_DIR
-sudo pm2 save
-sudo pm2 startup
+echo "Running the new container..."
+docker run -d --name $CONTAINER_NAME -p 3000:3000 $IMAGE_NAME
 
 echo "Deployment is completed 🚀"
