d8a77e2d56c73941863bdb5e0ee77e5dae0cd709
BGalymbek <200103023@stu.sdu.edu.kz>
2024-06-13 07:26:07 +0500

    deploy.sh fix process

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,82 @@
 #!/bin/bash
 
-set -e
+echo "Deleting old app..."
+sudo rm -rf /var/www/
 
-# Переменные
-IMAGE_NAME="***/frontamazon:latest"
-CONTAINER_NAME="myapp"
+echo "deleting old app"
+sudo rm -rf /var/www/html/*
 
-echo "Pulling the latest image..."
-docker pull $IMAGE_NAME
+echo "creating app folder"
+sudo mkdir -p /var/www/html/myapp
+sudo chown -R $EC2_USERNAME:$EC2_USERNAME /var/www/html/myapp
 
-echo "Stopping the existing container..."
-docker stop $CONTAINER_NAME || true
 
-echo "Removing the existing container..."
-docker rm $CONTAINER_NAME || true
 
-# Очистка Docker для освобождения места
-echo "Pruning Docker system to free up space..."
-docker system prune -a -f
+echo "cloning the latest version of the code"
+# Убедитесь, что у вас есть доступ к вашему репозиторию
+sudo git clone https://github.com/BGalymbek/FrontAmazon.git /var/www/html/myapp
 
-echo "Running the new container..."
-docker run -d --name $CONTAINER_NAME -p 3000:3000 $IMAGE_NAME
+cd /var/www/html/myapp
+
+echo "installing node and npm"
+# Установка Node.js и npm, если они еще не установлены
+if ! command -v node > /dev/null; then
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+
+fi
+
+npm install react-scripts@latest
+
+echo "installing project dependencies"
+sudo rm -rf node_modules
+sudo rm package-lock.json
+sudo npm install
+sudo npm install pm2@latest -g
+
+
+echo "building the project"
+sudo npm run build
+sudo pm2 serve build/ 80 --spa
+
+
+echo "moving build to root directory"
+sudo mv build/* /var/www/html/
+
+# Настройка Nginx для раздачи статических файлов
+if ! command -v nginx > /dev/null; then
+    echo "installing nginx"
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+sudo rm -f /etc/nginx/sites-available/default
+
+# Конфигурация сервера Nginx
+if [ ! -f /etc/nginx/sites-available/default ]; then
+    sudo bash -c 'cat > /etc/nginx/sites-available/default <<EOF
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        root /var/www/html;
+        index index.html index.htm;
+        try_files \$uri \$uri/ /index.html;
+    }
+}
+EOF'
+
+    sudo systemctl restart nginx
+    
+
+else
+    echo "Nginx configuration already exists."
+fi
+
+sudo systemctl status nginx
+cd /var/www/html/myapp
+sudo pm2 save
+sudo pm2 startup
 
 echo "Deployment is completed 🚀"
